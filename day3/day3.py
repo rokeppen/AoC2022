811c962304197a56f34130678a5a98b33347a244
rokeppen <robinkep@gmail.com>
2022-12-04 00:21:02 +0100

    Why did I just found out about this?

--- day3/day3.py
+++ day3/day3.py
@@ -1,8 +1,10 @@
-def priorities(chars):
-    print(sum(map(lambda c: ord(c.lower()) - 96 + 26 * c.isupper(), chars)))
+from aocd import lines, submit
+
+
+def prio(chars):
+    return sum(ord(c.lower()) - 96 + 26 * c.isupper() for c in chars)
 
 
 if __name__ == '__main__':
-    priorities((set(line[:len(line) // 2]) & set(line[len(line) // 2:])).pop() for line in open('input.txt'))
-    priorities(set.intersection(*map(lambda x: set(x.strip()), t)).pop() for t in zip(*[open('input.txt')] * 3))
-
+    submit(prio((set(l[:len(l) // 2]) & set(l[len(l) // 2:])).pop() for l in lines), 'a')
+    submit(prio(set.intersection(*map(set, lines[i:i + 3])).pop() for i in range(0, len(lines), 3)), 'b')
