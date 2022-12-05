6fa59912259956b6090c5aecfb4336e03195d59d
rokeppen <robinkep@gmail.com>
2022-12-06 00:57:20 +0100

    Day 5, but best

--- day5/day5.py
+++ day5/day5.py
@@ -3,15 +3,15 @@ from re import findall
 from copy import deepcopy
 
 
-def stack(proc, mover, is9001):
-    for amount, fr, to in [list(map(int, findall(r'\d+', p))) for p in proc]:
-        mover[to - 1].extend(mover[fr - 1][-amount:][::is9001])
+def stack(proc, mover, part):
+    for amount, fr, to in (map(int, findall(r'\d+', p)) for p in proc):
+        mover[to - 1].extend(mover[fr - 1][-amount:][::-1 if part == "a" else 1])
         del mover[fr - 1][-amount:]
-    submit("".join(s[-1] for s in mover), "b" if is9001 == 1 else "a")
+    submit("".join(s[-1] for s in mover), part)
 
 
 if __name__ == '__main__':
     cm, procedures = map(lambda x: x.split("\n"), data.split("\n\n"))
     cm = [[cm[j][i] for j in range(len(cm) - 1) if cm[j][i].strip()][::-1] for i in range(1, len(cm[0]), 4)]
-    stack(procedures, deepcopy(cm), -1)
-    stack(procedures, deepcopy(cm), 1)
+    stack(procedures, deepcopy(cm), "a")
+    stack(procedures, deepcopy(cm), "b")
