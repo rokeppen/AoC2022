1be545ed7c66543fad40e549d8f9d3597cd3e054
rokeppen <robinkep@gmail.com>
2022-12-05 21:57:07 +0100

    Day 5, but better

--- day5/day5.py
+++ day5/day5.py
@@ -3,16 +3,15 @@ from re import findall
 from copy import deepcopy
 
 
+def stack(proc, mover, is9001):
+    for amount, fr, to in [list(map(int, findall(r'\d+', p))) for p in proc]:
+        mover[to - 1].extend(mover[fr - 1][-amount:][::is9001])
+        del mover[fr - 1][-amount:]
+    submit("".join(s[-1] for s in mover), "b" if is9001 == 1 else "a")
+
+
 if __name__ == '__main__':
-    cm9000, procedures = data.split("\n\n")
-    cm9000, names = cm9000.rsplit("\n", 1)
-    cm9000 = cm9000.split("\n")
-    cm9000 = [[cm9000[j][i] for j in range(len(cm9000)) if cm9000[j][i].strip()][::-1] for i in range(1, len(names), 4)]
-    cm9001 = deepcopy(cm9000)
-    for amount, fr, to in [list(map(int, findall(r'\d+', procedure))) for procedure in procedures.split("\n")]:
-        for _ in range(amount):
-            cm9000[to - 1].append(cm9000[fr - 1].pop())
-        cm9001[to - 1].extend(cm9001[fr - 1][-amount:])
-        del cm9001[fr - 1][-amount:]
-    submit("".join(stack[-1] for stack in cm9000), "a")
-    submit("".join(stack[-1] for stack in cm9001), "b")
+    cm, procedures = map(lambda x: x.split("\n"), data.split("\n\n"))
+    cm = [[cm[j][i] for j in range(len(cm) - 1) if cm[j][i].strip()][::-1] for i in range(1, len(cm[0]), 4)]
+    stack(procedures, deepcopy(cm), -1)
+    stack(procedures, deepcopy(cm), 1)
